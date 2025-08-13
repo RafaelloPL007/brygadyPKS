0c96d3c18e0b0fa2e01dcb44f45401ea49f10029
RafaelloPL007 <rafal2007.sadek@gmail.com>
2025-08-13 02:52:05 +0200

    tt update

--- script/main.js
+++ script/main.js
@@ -1396,7 +1396,7 @@ function sortServicesPKS(a, b) {
 function getAllTrips() {
   $.ajax({
     type: "GET",
-    url: "script/json/" + urlBase + "trips.json?v=25-03-23v10",
+    url: "script/json/" + urlBase + "trips.json?v=25-03-23v11",
     dataType: "json"
   }).done(function (data) {
     totalTrips = data.length;
@@ -1435,7 +1435,7 @@ function getAllTrips() {
 function pobierzRezerwy() {
   $.ajax({
     type: "GET",
-    url: "script/json/" + urlBase + "rezerwy.json?v=25-03-23v10",
+    url: "script/json/" + urlBase + "rezerwy.json?v=25-03-23v11",
     dataType: "json"
   }).done(function (data) {
     data.forEach(rezerwa => {
@@ -2068,7 +2068,7 @@ function displayServiceDetails(id, tbodyEl) {
 function getLines(skip) {
   $.ajax({
     type: "GET",
-    url: "script/json/" + urlBase + "lines.json?v=25-03-23v10",
+    url: "script/json/" + urlBase + "lines.json?v=25-03-23v11",
     dataType: "json",
   }).done(function (data) {
     data.forEach((e) => {
@@ -2098,7 +2098,7 @@ function getLines(skip) {
 function getRoutes() {
   $.ajax({
     type: "GET",
-    url: "script/json/" + urlBase + "routesDist.json?v=25-03-23v10",
+    url: "script/json/" + urlBase + "routesDist.json?v=25-03-23v11",
     dataType: "json"
   }).done(function (data) {
     data.forEach((e) => {
@@ -2325,7 +2325,7 @@ function displayTripDetails(sId, tId, tbodyEl) {
 function getVehicleList() {
   $.ajax({
     type: "GET",
-    url: "script/json/" + urlBase + "vehicles.json?v=25-03-23v10",
+    url: "script/json/" + urlBase + "vehicles.json?v=25-03-23v11",
     dataType: "json",
   }).done(function (data) {
     vehicleList = data;
@@ -3200,12 +3200,12 @@ function getPKSServices() {
 function getPKSTrips() {
   $.ajax({
     type: "GET",
-    url: "script/json/" + urlBasePKS + "trips.json?v=25-03-23v10",
+    url: "script/json/" + urlBasePKS + "trips.json?v=25-03-23v11",
     dataType: "json"
   }).done(function (trips) {
     $.ajax({
       type: "GET",
-      url: "script/json/" + urlBasePKS + "tripDetails.json?v=25-03-23v10",
+      url: "script/json/" + urlBasePKS + "tripDetails.json?v=25-03-23v11",
       dataType: "json"
     }).done(function (tripDets) {
       trips.forEach(trip => {
